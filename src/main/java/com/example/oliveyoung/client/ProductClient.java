96e7adb406b5684d8c9397d942c9f0576962c53a
jaebinGit <tnwoql327@gmail.com>
2024-08-28 16:00:44 +0900

    Refactor : msa 적용

--- src/main/java/com/example/oliveyoung/client/ProductClient.java
+++ src/main/java/com/example/oliveyoung/client/ProductClient.java
@@ -3,7 +3,7 @@ package com.example.oliveyoung.client;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 
-@FeignClient(name = "service-products", url = "https://api.hachwimu.com")
+@FeignClient(name = "products", url = "https://api.hachwimu.com")
 public interface ProductClient {
 
     @PostMapping("/products/cache/clear")
