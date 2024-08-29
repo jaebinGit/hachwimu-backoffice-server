b56d4c9a0b6405477a7850d1fc6f26c0c2dd3485
jaebinGit <tnwoql327@gmail.com>
2024-08-30 02:01:26 +0900

    Docs : last commit

--- src/main/java/com/example/oliveyoung/config/WebConfig.java
+++ src/main/java/com/example/oliveyoung/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")  // API 경로를 지정
-                .allowedOrigins("*") //https://www.hachwimu.com
+                .allowedOrigins("*") //https://admin.hachwimu.com
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(false);  // 자격 증명을 허용 (쿠키, 인증 헤더 전송)
